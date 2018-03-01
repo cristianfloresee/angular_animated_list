a457edccb6f149150e630f727c8042f2bec70b0b
Cristian Flores <cristianflores.ee@gmail.com>
2018-03-01 17:43:13 -0300

    list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TransitionGroupComponent, TransitionGroupItemDirective } from './transition-group/transition-group.component';
+
+declare let _: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  shufflse() {
+    //this.items = _.shuffle(this.items)
+  }
+
+  shuffle() {
+    for (var i = this.items.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = this.items[i];
+        this.items[i] = this.items[j];
+        this.items[j] = temp;
+    }
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TransitionGroupComponent, TransitionGroupItemDirective } from './transition-group/transition-group.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransitionGroupComponent,
+    TransitionGroupComponent,
+    TransitionGroupItemDirective
   ],
   imports: [
     BrowserModule
