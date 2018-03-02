65522e907dab8f6140ed9b5e1cf9d46131fc2e75
Cristian Flores <cristianflores.ee@gmail.com>
2018-03-01 22:05:57 -0300

    solid

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,15 +6,8 @@ import { setTimeout, setInterval } from 'timers';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent {
 
-  constructor(){
-    setInterval(()=>{    //<<<---    using ()=> syntax
-      this.shuffle();
- },1000);
-  }
-
-  //items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   items = [
     { data: [20], label: 'Arica' },
     { data: [23], label: 'Iquique' },
@@ -22,16 +15,12 @@ export class HomeComponent  {
     { data: [34], label: 'Chopiri' },
   ];
 
-  shufflse() {
-    //this.items = _.shuffle(this.items)
-  }
-
   shuffle() {
     for (var i = this.items.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = this.items[i];
-        this.items[i] = this.items[j];
-        this.items[j] = temp;
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.items[i];
+      this.items[i] = this.items[j];
+      this.items[j] = temp;
     }
 
   }
